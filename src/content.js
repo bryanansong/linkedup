fc13352945cdb5060cda424bb90c0242ed37da5c
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-05-19 18:59:18 -0400

    Stored test data in content.js

---
+++ src/content.js
@@ -0,0 +1,19 @@
+// Test Data for profiles
+// {
+// 	name: "Bryan Ansong",
+// 	profileUrl: "https://www.linkedin.com/in/bryanansong/",
+// 	imageUrl:
+// 		"https://media.licdn.com/dms/image/D4E03AQGRu16ltFUVVw/profile-displayphoto-shrink_400_400/0/1713572003638?e=1721865600&v=beta&t=91Hn3aF77oVmBo6HP1_dioZge5mv7x5LxClAzh2CdYc",
+// },
+// {
+// 	name: "Micah Fossett",
+// 	profileUrl: "https://www.linkedin.com/in/micahfossett/",
+// 	imageUrl:
+// 		"https://media.licdn.com/dms/image/D5603AQGa0iDn8kPNMg/profile-displayphoto-shrink_400_400/0/1683279740366?e=1721865600&v=beta&t=yFA3fWNsAQbDMYUXZeJNExfkUgQcfhy1_UslzNVYhDg",
+// },
+// {
+// 	name: "Edith Kao",
+// 	profileUrl: "https://www.linkedin.com/in/edith-kao/",
+// 	imageUrl:
+// 		"https://media.licdn.com/dms/image/D4E03AQGK0VWqp-B3XA/profile-displayphoto-shrink_400_400/0/1713902585796?e=1721865600&v=beta&t=g1OY5vHbmzdJDNXL7DtESCnLDRpju3hxbVXZ0x9_QRo",
+// },
