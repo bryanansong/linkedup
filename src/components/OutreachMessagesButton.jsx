a241ba79263deaf320bd1ad0fb4f3dcb0302a1a3
Bryan Ansong <50881304+bryanansong@users.noreply.github.com>
2024-06-01 14:33:13 -0600

    Updated outreach messages link

--- src/components/OutreachMessagesButton.jsx
+++ src/components/OutreachMessagesButton.jsx
@@ -1,6 +1,6 @@
 const OutreachMessagesButton = () => {
-	// TODO: Replace this with correct URL
-	const outreachMessagesUrl = "www.bryanansong.com";
+	const outreachMessagesUrl =
+		"https://roasted-gastonia-25f.notion.site/LinkedUp-Networking-Connection-Messages-1e89f246fd4047dda39d2e35c1f478a5?pvs=4";
 	return (
 		<div
 			className="outreach-messages-button"
